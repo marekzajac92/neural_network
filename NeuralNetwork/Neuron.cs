3be75cb4a909bb17c338b191c175cecf53886605
Marek Zając <contact@zajacmarek.com>
2015-11-15 11:13:10 +0100

    Implemented methods in Neuron class

--- NeuralNetwork/Neuron.cs
+++ NeuralNetwork/Neuron.cs
@@ -1,33 +1,55 @@
 ﻿using System;
+using System.Linq;
 using NeuralNetwork.Interfaces;
 
 namespace NeuralNetwork
 {
     public class Neuron
     {
+        private readonly int _numberOfInputs;
+        private readonly float[] _inputs;
+        private readonly float[] _weights;
+        private readonly IActivationFunction _activationFunction;
+
         public Neuron(int numberOfInputs, IActivationFunction activationFunction)
         {
-            throw new NotImplementedException();
+            _numberOfInputs = numberOfInputs;
+            _activationFunction = activationFunction;
+            
+            _inputs = new float[numberOfInputs];
+            _weights = new float[numberOfInputs];
         }
 
         public void SetInputValue(int i, float value)
         {
-            throw new NotImplementedException();
+            if(i >= _numberOfInputs || i < 0)
+                throw new ArgumentOutOfRangeException();
+
+            _inputs[i] = value;
         }
 
         public void SetWeight(int i, float value)
         {
-            throw new NotImplementedException();
+            if(i >= _numberOfInputs || i < 0)
+                throw new ArgumentOutOfRangeException();
+
+            _weights[i] = value;
         }
 
         public float GetInputValue(int i)
         {
-            throw new NotImplementedException();
+            if(i >= _numberOfInputs || i < 0)
+                throw new ArgumentOutOfRangeException();
+
+            return _inputs[i];
         }
 
         public float GetWeight(int i)
         {
-            throw new NotImplementedException();
+            if (i >= _numberOfInputs || i < 0)
+                throw new ArgumentOutOfRangeException();
+
+            return _weights[i];
         }
 
         public float GetMaxValue()
@@ -42,7 +64,26 @@ namespace NeuralNetwork
 
         public float GetValue()
         {
-            throw new NotImplementedException();
+            var sum = SumInputs();
+            var weightsSum = SumWeights();
+
+            return _activationFunction.GetValue(sum, weightsSum);
+        }
+
+        private float SumInputs()
+        {
+            var sum = 0.0f;
+            for (var i = 0; i < _numberOfInputs; i++)
+            {
+                sum += _inputs[i]*_weights[i];
+            }
+
+            return sum;
+        }
+
+        private float SumWeights()
+        {
+            return _weights.Sum();
         }
     }
 }
