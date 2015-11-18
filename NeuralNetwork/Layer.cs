0c2d9aa82bb8d2172906f8e95ab3ae197175eac5
Marek Zając <marekzajac92@gmail.com>
2015-11-18 20:06:02 +0100

    Implemented later class

--- NeuralNetwork/Layer.cs
+++ NeuralNetwork/Layer.cs
@@ -1,34 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using NeuralNetwork.Interfaces;
 
 namespace NeuralNetwork
 {
     public class Layer
     {
+        private readonly int _numberOfInputs;
+        private readonly Neuron[] _neurons;
+        private readonly IEnumerable<Connection> _connections;
+        private readonly IScalingMethod _scalingMethod;
+
         public Layer(int numberOfInputs, Neuron[] neurons, IEnumerable<Connection> connections, IScalingMethod scalingMethod)
         {
-            throw new NotImplementedException();
+            _numberOfInputs = numberOfInputs;
+            _neurons = neurons;
+
+            _connections = connections;
+            _scalingMethod = scalingMethod;
         }
 
         public void SetInput(float[] values)
         {
-            throw new NotImplementedException();
+            if(values.Length != _numberOfInputs)
+                throw new ArgumentException();
+
+            for (var i = 0; i < values.Length; i++)
+            {
+                var connections = _connections.Where(x => x.Value == i);
+
+                foreach (var connection in connections)
+                {
+                    _neurons[connection.Neuron].SetInputValue(connection.NeuronInput, _scalingMethod.Scale(values[i]));
+                }
+            }
         }
 
         public float[] GetOutput()
         {
-            throw new NotImplementedException();
+            var output = _neurons.Select(x => x.GetValue()).ToArray();
+
+            return output;
         }
 
         public int GetNumberOfInputs()
         {
-            throw new NotImplementedException();
+            return _numberOfInputs;
         }
 
         public int GetNumberOfOutputs()
         {
-            throw new NotImplementedException();
+            return _neurons.Length;
         }
     }
 }
